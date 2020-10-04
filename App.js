6082983221410fb32defb8cbc7fc6e9c023c69c9
Elizaveta Dolgova <dolgovadesign@gmail.com>
2020-10-03 23:36:06 -0400

    Add last changes

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign, Entypo, FontAwesome, MaterialIcons, Feather } from "@expo/vector-icons";
-import { Text, 
-         View, 
-         Button, 
-         Alert, 
-         Image, 
-         Dimensions, 
-         AsyncStorage,
-         StyleSheet
-        } from "react-native";
-
+import { Entypo, FontAwesome, MaterialIcons, Feather } from "@expo/vector-icons";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 import FadeIn from "./screens/FadeIn";
@@ -29,48 +18,35 @@ const RootTabNavigator = () => {
         tabBarOptions={{
           activeTintColor: "darkgreen",
           inactiveTintColor: "gray",
-     }}
-      
-      >
-      <RootTab.Screen name="FadeIn" 
-                      component={FadeIn}
+     }}>
+      <RootTab.Screen name="FadeIn" component={FadeIn}
                       options={{
                         title: "Quote",
                         tabBarIcon: ({ focused }) => (
                           <Entypo name="open-book" size={24} color={focused ? "darkgreen" : "gray"} />
-                        ),
-                      }}
-        />
-      <RootTab.Screen name="Slideshow" 
-                      component={Details}
+                        )
+                      }} />
+      <RootTab.Screen name="Slideshow" component={Details}
                       options={{
                         title: "Slideshow",
                         tabBarIcon: ({ focused }) => (
                           <FontAwesome name="camera-retro" size={24} color={focused ? "darkgreen" : "gray"} />
-                        ),
-                      }}
-                      />
-
-<RootTab.Screen name="Rotate" 
-                      component={Rotate}
+                        )
+                      }} />
+      <RootTab.Screen name="Rotate" component={Rotate}
                       options={{
                         title: "Rotate",
                         tabBarIcon: ({ focused }) => (
                           <MaterialIcons name="rotate-right" size={24} color={focused ? "darkgreen" : "gray"} />
-                        ),
-                      }}
-                      />
-
-<RootTab.Screen name="DragGesture" 
-                component={DragGesture}
-                options={{
-                title: "Play",
-                  tabBarIcon: ({ focused }) => (
-                    <Feather name="play-circle" size={24} color={focused ? "darkgreen" : "gray"} />
-                  ),
-                }}
-                />
-
+                        )
+                      }} />
+      <RootTab.Screen name="DragGesture" component={DragGesture}
+                      options={{
+                      title: "Play",
+                        tabBarIcon: ({ focused }) => (
+                          <Feather name="play-circle" size={24} color={focused ? "darkgreen" : "gray"} />
+                        )
+                      }} />
     </RootTab.Navigator>       
   );
 };
--- screens/DragGesture.js
+++ screens/DragGesture.js
@@ -1,44 +1,46 @@
 import React, { useRef, useEffect } from "react";
-import { Text, Animated, View, StyleSheet, PanResponder } from "react-native";
+import { Dimensions, Text, Animated, View, StyleSheet, PanResponder } from "react-native";
 import { useIsFocused } from "@react-navigation/native";
 
 export default DragGesture = () => {
-    const position = useRef(new Animated.ValueXY()).current;
-    const isFocused = useIsFocused();
-    
-    const panResponder = PanResponder.create({
-       onMoveShouldSetPanResponderCapture:() => true,
-       onPanResponderMove: Animated.event([
-           null,
-           { dx: position.x, dy: position.y },
-       ]),
-       onPanResponderGrant: ()=>{
-           position.setOffset({
-               x: position.x._value,
-               y: position.y._value,
-           });
-       },
-     //onPanResponderRelease: () => {
-     // position.flattenOffset();
-     //},
-     //});
-       onPanResponderRelease: () => {
-        Animated.spring(position, {
+  const isFocused = useIsFocused();
+  const screen = Dimensions.get("window");
+  const bottomPanelHeight = 40;
+  const ballOffsetY = 11;
+  const centerY = (screen.height / 2) - (styles.outerball.height / 2) - bottomPanelHeight;
+  const centerX = (screen.width / 2) - (styles.outerball.width / 2);
+  const position = new Animated.ValueXY({ x: 0, y: ballOffsetY });
+
+  //position.addListener((p) => console.log(`x: ${p.x}, y: ${p.y}`));
+
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponderCapture: () => true,
+    onPanResponderMove: Animated.event([null, { dx: position.x, dy: position.y }], { useNativeDriver: false }),
+    onPanResponderGrant: () => position.setOffset({ x: position.x._value,y: ballOffsetY, }),
+    onPanResponderRelease: () => {
+      Animated.spring(position, {
         toValue: { x: 0, y: 0 },
-       }).start();    
+        useNativeDriver: false
+      }).start();
     },
-    });
+  });
 
-       useEffect(() => {
-        return ()=> position.setValue({ x: 0, y: 0});
-   }, [isFocused]);
+  useEffect(() => {
+        return () => position.setValue({ x: 0, y: 0 });
+    },
+    [isFocused]);
 
-    return (
-        <View style={styles.container}>
-            <Animated.View style={[styles.ball, position.getLayout()]} 
-                           {...panResponder.panHandlers} />
+  return (
+    <View style={styles.container}>
+        <View style={[styles.outerball, { top: centerY, right: centerX}]}>
+            <Animated.View
+                style={[styles.ball, position.getLayout()]}
+                {...panResponder.panHandlers}>
+                <Text style={styles.text}>)|(</Text>
+            </Animated.View>
         </View>
-    );
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -49,9 +51,29 @@ const styles = StyleSheet.create({
         backgroundColor: "darkgreen",
     },
     ball:{
-        backgroundColor: "white",
-        height: 34,
-        width: 34,
+        backgroundColor: "orange",
+        height: 65,
+        width: 65,
         borderRadius: 50,
+        alignContent: "center",
+        position: "relative"
     },
-    });
+    outerball: {
+        backgroundColor: "darkgreen",
+        height: 100,
+        width: 100,
+        position: "absolute",
+        borderRadius: 50,
+        borderWidth: 5,
+        borderStyle: "dashed",
+        borderColor: "white",
+        alignItems: "center"
+    },
+    text: {
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 50,
+        marginTop: -2,
+        color: "darkgreen"
+    }
+});
